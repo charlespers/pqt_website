7980db126d3db32bf76f56b6adae5449bb78b865
charlespers <charlespers.m@gmail.com>
2025-02-26 12:21:35 -0500

    wrapped page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 export default function Home() {
   return (
-    <div className="text-center py-20">
+    <div className="flex flex-col items-center justify-center min-h-screen text-center">
       <h2 className="text-4xl font-extrabold">Welcome to Princeton Quantitative Traders</h2>
-      <p className="mt-4 text-lg text-gray-400">We leverage quantitative methods to gain a competitive edge in trading.</p>
+      <p className="mt-4 text-lg text-gray-400">
+        We leverage quantitative methods to gain a competitive edge in trading.
+      </p>
       <div className="mt-6">
         <a href="/mission" className="bg-blue-500 px-6 py-2 rounded-full text-white font-bold">Learn More</a>
       </div>
