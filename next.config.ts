1000f841ebb4659e5f6678958ea63401aeb42f25
charlespers <charlespers.m@gmail.com>
2025-02-26 12:44:44 -0500

    i hate js

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,7 @@ const nextConfig = {
         headers: [
           {
             key: "Content-Security-Policy",
-            value: "default-src 'self'; script-src 'self' https://trustedsource.com; object-src 'none'; base-uri 'self';",
+            value: "default-src 'self'; script-src 'self' 'nonce-random123' https://trustedsource.com; object-src 'none'; base-uri 'self';",
           },
         ],
       },
