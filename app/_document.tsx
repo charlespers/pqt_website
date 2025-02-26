8602a6270b090658c3af33e26391becf5548813b
charlespers <charlespers.m@gmail.com>
2025-02-26 13:31:05 -0500

    changed document

--- app/_document.tsx
+++ app/_document.tsx
@@ -7,7 +7,7 @@ export default function Document() {
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta httpEquiv="Content-Security-Policy" content="default-src 'self'; script-src 'self' https://trustedsource.com; object-src 'none'; base-uri 'self';" />
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha384-..." crossorigin="anonymous" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha384-..." crossOrigin="anonymous" />
       </Head>
       <body>
         <Main />
