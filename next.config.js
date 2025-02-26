73e2749cc10d51f3faa81dbf6836d327b3e94ec7
charlespers <charlespers.m@gmail.com>
2025-02-26 14:14:11 -0500

    ts pmo

--- next.config.js
+++ next.config.js
@@ -1,18 +1,20 @@
 /** @type {import('next').NextConfig} */
+const crypto = require("crypto");
+
 const nextConfig = {
-    async headers() {
-      return [
-        {
-          source: "/:path*",
-          headers: [
-            {
-              key: "Content-Security-Policy",
-              value: "default-src 'self'; style-src 'self' 'unsafe-inline' 'nonce-random123';",
-            },
-          ],
-        },
-      ];
-    },
-  };
-  
-  module.exports = nextConfig;
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: `default-src 'self'; style-src 'self' 'nonce-${crypto.randomBytes(16).toString("base64")}' 'unsafe-inline';`,
+          },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
