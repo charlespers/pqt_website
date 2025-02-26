bf9e6ac5289e2db0eca6efe13fddb86129515c28
charlespers <charlespers.m@gmail.com>
2025-02-26 14:55:37 -0500

    g

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,15 +1,13 @@
 import Link from "next/link";
+
 export default function Navbar() {
   return (
-    <nav className="bg-gray-800 p-4 flex justify-between">
-      <h1 className="text-xl font-bold">Princeton QT</h1>
-      <div>
-        <Link href="/" className="mx-2">Home</Link>
-        <Link href="/mission" className="mx-2">Mission</Link>
-        <Link href="/members" className="mx-2">Members</Link>
-        <Link href="/sponsors" className="mx-2">Sponsors</Link>
-        <Link href="/competitions" className="mx-2">Competitions</Link>
-        <Link href="/contact" className="mx-2">Contact</Link>
+    <nav className="navbar">
+      <div className="flex gap-6">
+        <Link href="/">Home</Link>
+        <Link href="/about">About</Link>
+        <Link href="/contact">Contact</Link>
+        <Link href="/TradingGraph">Trading</Link>
       </div>
     </nav>
   );
