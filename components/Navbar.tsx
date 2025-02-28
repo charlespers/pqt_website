2c423855e994c0f17c6bb3c6f7a8cbf58be1f566
charlespers <charlespers.m@gmail.com>
2025-02-28 17:21:55 -0500

    major

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,14 +8,14 @@ export default function Navbar() {
 
   return (
     <nav className="navbar fixed top-0 left-0 w-full z-50">
-      <div className="navbar-container flex items-center justify-between w-full max-w-6xl mx-auto px-4 py-3">
+      <div className="navbar-container flex items-center justify-between w-full max-w-6xl mx-auto px-4 py-2">
         {/* Logo */}
         <Link href="/">
-          <Image src="/PQT-Logo1.png" alt="PQT Logo" width={50} height={50} />
+          <Image src="/PQT-Logo1.png" alt="PQT Logo" width={40} height={40} />
         </Link>
 
-        {/* Title */}
-        <h1 className="text-xl sm:text-2xl font-bold text-orange-500 text-center flex-grow mx-4">
+        {/* Title - Reduced Font Size */}
+        <h1 className="text-lg sm:text-xl font-bold text-orange-500 text-center flex-grow mx-4">
           Princeton Quantitative Traders
         </h1>
 
