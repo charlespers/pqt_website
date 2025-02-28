3d19b48a82d054b4ec7a7c40aa5fde031cd5d5f2
charlespers <charlespers.m@gmail.com>
2025-02-28 18:08:54 -0500

    page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,8 @@
+"use client";
+import "./globals.css";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import FractalBackground from "./FractalBackground";
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
