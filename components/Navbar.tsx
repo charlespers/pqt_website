7838a74afae629f99d9fba69dd6d595b698526c3
charlespers <charlespers.m@gmail.com>
2025-02-28 13:38:22 -0500

    up

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,3 @@
-"use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
@@ -6,10 +5,17 @@ export default function Navbar() {
   const pathname = usePathname();
 
   return (
-    <nav className="navbar-links">
-      <Link href="/" className={pathname === "/" ? "active" : ""}>Home</Link>
-      <Link href="/about" className={pathname === "/about" ? "active" : ""}>About</Link>
-      <Link href="/contact" className={pathname === "/contact" ? "active" : ""}>Contact</Link>
+    <nav className="navbar-container">
+      <div className="logo">
+        <Link href="/">
+          <img src="/PQT-Logo1.png" alt="PQT Logo" />
+        </Link>
+      </div>
+      <div className="navbar-links">
+        <Link href="/" className={pathname === "/" ? "active" : ""}>Home</Link>
+        <Link href="/about" className={pathname === "/about" ? "active" : ""}>About</Link>
+        <Link href="/contact" className={pathname === "/contact" ? "active" : ""}>Contact</Link>
+      </div>
     </nav>
   );
 }
