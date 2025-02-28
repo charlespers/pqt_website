8eb55866d57f5b4d0228b7822a19cfffe4e4376b
charlespers <charlespers.m@gmail.com>
2025-02-28 15:03:24 -0500

    changes to css

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,19 +4,49 @@ import "./globals.css";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import FractalBackground from "./FractalBackground";
+import Image from "next/image";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
+        {/* Fractal Background - Properly layered behind everything */}
         <FractalBackground />
+
+        {/* Navigation Bar */}
         <Navbar />
 
-        {/* Stacked sections with centered content */}
-        <main className="main-container">
-          <div className="content-box">{children}</div>
+        {/* Main Content */}
+        <main className="flex flex-col items-center min-h-screen px-4">
+          <div className="content-box">
+            {/* Princeton QT Logo - Centered */}
+            <div className="flex justify-center">
+              <Image src="/PQT-Logo1.png" alt="PQT Logo" width={200} height={200} />
+            </div>
+
+            {/* Mission Statement */}
+            <p className="text-lg mt-4">
+              Our mission is to bridge the gap between Princeton students and quantitative finance 
+              through education, research, and competitions.
+            </p>
+
+            {/* Princeton Tiger Image (Should be transparent) */}
+            <div className="flex justify-center mt-4">
+              <Image 
+                src="/princeton-tiger.png" 
+                alt="Princeton Tiger" 
+                width={200} 
+                height={200} 
+                className="princeton-tiger" 
+              />
+            </div>
+
+            {/* Inject Page-Specific Content */}
+            <div className="page-content">{children}</div>
+          </div>
         </main>
 
+        {/* Footer */}
         <Footer />
       </body>
     </html>
