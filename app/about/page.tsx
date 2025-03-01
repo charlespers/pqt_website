88c7f4250b84eefd6ed949f1665cdd4acd0b36a9
charlespers <charlespers.m@gmail.com>
2025-02-28 19:05:38 -0500

    page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -8,15 +8,33 @@ export default function About() {
 
         {/* Grid container with proper spacing */}
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-10 w-full">
-          {["Competitions", "Projects", "Research", "Education"].map((section) => (
+          {[
+            {
+              title: "Competitions",
+              description: "Coming Soon"
+            },
+            {
+              title: "Projects",
+              description:
+                "Students will get into groups and work on real-industry quantitative projects such as building machine learning models, integrating live data to create algorithms on exchanges, targeting arbitrage in markets, and market-making projects using statistics, data science tools, and language, with the guidance of professional mentors."
+            },
+            {
+              title: "Research",
+              description:
+                "Students will experiment with different trading strategies, time series analysis, pricing models, derivatives, optimization schemes, machine learning theory, and compile results into iterative research models, driving the cutting edge of financial analytics."
+            },
+            {
+              title: "Education",
+              description:
+                "Our curriculum is beginner-friendly, designed for those with no prior experience in quantitative trading. We teach how to learn, how to prepare for interviews, and provide essential quantitative trading know-how to build a strong foundation in the field."
+            }
+          ].map(({ title, description }) => (
             <div
-              key={section}
+              key={title}
               className="about-box bg-black/30 p-6 rounded-xl shadow-lg flex flex-col"
             >
-              <h3 className="text-2xl font-semibold text-orange-400">{section}</h3>
-              <p className="text-white mt-2 flex-grow">
-                Description of {section} initiatives.
-              </p>
+              <h3 className="text-2xl font-semibold text-orange-400">{title}</h3>
+              <p className="text-white mt-2 flex-grow">{description}</p>
             </div>
           ))}
         </div>
